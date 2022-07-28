5a3568c023f5493f840c3fd622279bc80be01e0d
ericrothmuller <eric@skymarketingco.com>
2022-07-28 00:53:02 -0700

    Added schema.sql initial table creation code for all 3 tables, department, roles, and employees.

---
+++ schema.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTS employees_db;
+CREATE DATABASE employees_db;
+
+USE employees_db;
+
+CREATE TABLE department (
+  id INT AUTO_INCREMENT,
+  department VARCHAR(30) NOT NULL,
+);
+
+CREATE TABLE roles (
+  id INT AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  department VARCHAR(30) NOT NULL,
+  salary DECIMAL(7,2) NOT NULL,
+);
+
+CREATE TABLE employees (
+  id INT AUTO_INCREMENT,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  title VARCHAR(30) NOT NULL,
+  department VARCHAR(30) NOT NULL,
+  salary DECIMAL(7,2) NOT NULL,
+  manager VARCHAR(30) NOT NULL,
+);
