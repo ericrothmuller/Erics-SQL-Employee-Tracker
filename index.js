ae4b40714abdf31e67ac16b91ea0358034408ae2
ericrothmuller <eric@skymarketingco.com>
2022-07-28 00:32:54 -0700

    Added functions with comments for the prompts with placeholder console.logs until I can complete the functions. I'll need to setup the database first before I can finish these functions.

--- index.js
+++ index.js
@@ -1,29 +1,79 @@
 const inquirer = require("inquirer");
 const db = require("./db");
 
-inquirer
-.prompt([
-   {
-       type: "list",
-       message: "What would you like to do?",
-       name: "whatToDo",
-       choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
-   }
-])
-.then ((response) => {
-    if (response.whatToDo === "View all departments") {
-        viewDepartments();
-    } else if (response.whatToDo === "View all roles") {
-        viewRoles();
-    } else if (response.whatToDo === "View all employees") {
-        viewEmployees();
-    } else if (response.whatToDo === "Add a department") {
-        addDepartment();
-    } else if (response.whatToDo === "Add a role") {
-        addRole();
-    } else if (response.whatToDo === "Add an employee") {
-        addEmployee();
-    } else if (response.whatToDo === "Update an employee role") {
-        updateEmployee();
-    } 
-});
+// Initial prompt
+
+
+function whatToDo() {
+    inquirer
+        .prompt([
+            {
+                type: "list",
+                message: "What would you like to do?",
+                name: "whatToDo",
+                choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
+            }
+        ])
+        .then((response) => {
+            if (response.whatToDo === "View all departments") {
+                viewDepartments();
+            } else if (response.whatToDo === "View all roles") {
+                viewRoles();
+            } else if (response.whatToDo === "View all employees") {
+                viewEmployees();
+            } else if (response.whatToDo === "Add a department") {
+                addDepartment();
+            } else if (response.whatToDo === "Add a role") {
+                addRole();
+            } else if (response.whatToDo === "Add an employee") {
+                addEmployee();
+            } else if (response.whatToDo === "Update an employee role") {
+                updateEmployee();
+            }
+        });
+}
+
+// View Departments
+
+function viewDepartments() {
+    console.log("This was ran");
+}
+
+// View Roles
+
+function viewRoles() {
+    console.log("This was ran");
+}
+
+// View Employees
+
+function viewEmployees() {
+    console.log("This was ran");
+}
+
+// Add Department
+
+function addDepartment() {
+    console.log("This was ran");
+}
+
+// Add Role
+
+function addRole() {
+    console.log("This was ran");
+}
+
+// Add Employee
+
+function addEmployee() {
+    console.log("This was ran");
+}
+
+// Update Employee
+
+function updateEmployee() {
+    console.log("This was ran");
+}
+
+
+
