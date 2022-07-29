77fa08e26fd85c0559d270ac38a990b5a47e1986
ericrothmuller <eric@skymarketingco.com>
2022-07-28 22:36:31 -0700

    Added addRole function to add a new role and assign it to a department

--- connection.js
+++ connection.js
@@ -1,15 +1,15 @@
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 
 // Connect to database
 const db = mysql.createConnection(
-    {
-      host: 'localhost',
-      user: 'root',
-      port: 3306,
-      password: 'password',
-      database: 'employees_db'
-    },
-    console.log(`Connected to the employees_db database.`)
-  );
+  {
+    host: "localhost",
+    user: "root",
+    port: 3306,
+    password: "password",
+    database: "employees_db",
+  },
+  console.log(`Connected to the employees_db database.`)
+);
 
-  module.exports = db;
+module.exports = db;
--- index.js
+++ index.js
@@ -1,117 +1,178 @@
 const inquirer = require("inquirer");
-const mysql = require('mysql2');
+const mysql = require("mysql2");
 const cTable = require("console.table");
 const db = require("./connection");
 
 // Starts Connection
 
 db.connect(function (err) {
-    if (err) throw err;
-    whatToDo();
+  if (err) throw err;
+  whatToDo();
 });
 
 // Initial prompt
 
-
 function whatToDo() {
-    inquirer
-        .prompt([
-            {
-                type: "list",
-                message: "What would you like to do?",
-                name: "whatToDo",
-                choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"]
-            }
-        ])
-        .then((response) => {
-            if (response.whatToDo === "View all departments") {
-                viewDepartments();
-            } else if (response.whatToDo === "View all roles") {
-                viewRoles();
-            } else if (response.whatToDo === "View all employees") {
-                viewEmployees();
-            } else if (response.whatToDo === "Add a department") {
-                addDepartment();
-            } else if (response.whatToDo === "Add a role") {
-                addRole();
-            } else if (response.whatToDo === "Add an employee") {
-                addEmployee();
-            } else if (response.whatToDo === "Update an employee role") {
-                updateEmployee();
-            }
-        });
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        message: "What would you like to do?",
+        name: "whatToDo",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+        ],
+      },
+    ])
+    .then((response) => {
+      if (response.whatToDo === "View all departments") {
+        viewDepartments();
+      } else if (response.whatToDo === "View all roles") {
+        viewRoles();
+      } else if (response.whatToDo === "View all employees") {
+        viewEmployees();
+      } else if (response.whatToDo === "Add a department") {
+        addDepartment();
+      } else if (response.whatToDo === "Add a role") {
+        addRole();
+      } else if (response.whatToDo === "Add an employee") {
+        addEmployee();
+      } else if (response.whatToDo === "Update an employee role") {
+        updateEmployee();
+      }
+    });
 }
 
 // View Departments
 
 function viewDepartments() {
-    db.query("SELECT * from department;", (err, table) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.table("All Departments:", table);
-          whatToDo();
-        }
-      });
+  db.query("SELECT * from department;", (err, table) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.table("All Departments:", table);
+      whatToDo();
+    }
+  });
 }
 
 // View Roles
 
 function viewRoles() {
-    db.query("SELECT * from role;", (err, table) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.table("All Roles:", table);
-          whatToDo();
-        }
-      });
+  db.query("SELECT * from role;", (err, table) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.table("All Roles:", table);
+      whatToDo();
+    }
+  });
 }
 
 // View Employees
 
 function viewEmployees() {
-    db.query("SELECT * from employee;", (err, table) => {
-        if (err) {
-          console.log(err);
-        } else {
-          console.table("All Employees:", table);
-          whatToDo();
-        }
-      });
+  db.query("SELECT * from employee;", (err, table) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.table("All Employees:", table);
+      whatToDo();
+    }
+  });
 }
 
 // Add Department
 
 function addDepartment() {
-    inquirer
+  inquirer
     .prompt([
-    {
+      {
         type: "input",
         message: "What is the department name?",
         name: "departmentName",
-    },
-]).then((response) => {
-    db.query("INSERT INTO department (name) VALUES (?)", response.departmentName);
-    console.log("Department added");
-    whatToDo();
-});
+      },
+    ])
+    .then((response) => {
+      db.query(
+        "INSERT INTO department (name) VALUES (?)",
+        response.departmentName
+      );
+      console.log("Department added");
+      whatToDo();
+    });
 }
 // Add Role
 
 function addRole() {
-    console.log("This was ran");
+  var departmentAll = "SELECT * FROM department";
+
+  var departmentNames = [];
+
+  var allDepartments = [];
+
+  db.query(departmentAll, (err, results) => {
+    for (var i = 0; i < results.length; i++) {
+      allDepartments.push(results[i]);
+      departmentNames.push(allDepartments[i].name);
+    }
+
+    inquirer
+      .prompt([
+        {
+          type: "input",
+          message: "What is the role name?",
+          name: "roleName",
+        },
+        {
+          type: "input",
+          message: "What is the role's salary?",
+          name: "roleSalary",
+        },
+        {
+          type: "list",
+          message: "What department does this role belong to?",
+          choices: departmentNames,
+          name: "roleDepartment",
+        },
+      ])
+      .then((response) => {
+        var departmentAll =
+          "INSERT INTO role (title, salary, department_id) VALUES (?, ?, ?)";
+        var selectedID;
+
+        for (var m = 0; m < allDepartments.length; m++) {
+          if (allDepartments[m].name === response.roleDepartment) {
+            selectedID = allDepartments[m].id;
+          }
+        }
+
+        db.query(
+          departmentAll,
+          [response.roleName, response.roleSalary, selectedID],
+          () => {
+            console.log("Role added");
+            whatToDo();
+          }
+        );
+      });
+  });
 }
 
 // Add Employee
 
 function addEmployee() {
-    console.log("This was ran");
+  console.log("This was ran");
 }
 
 // Update Employee
 
 function updateEmployee() {
-    console.log("This was ran");
+  console.log("This was ran");
 }
-
